64c21a13c72e5b4d3acda23761d61adac04803d0
mconiaris <mconiaris@gmail.com>
2014-09-24 20:16:18 -0400

    Added male? and female? methods to user.rb

--- models/user.rb
+++ models/user.rb
@@ -2,9 +2,15 @@ class User < Sequel::Model
   # many_to_many(:viewers)
   # one_to_one(:recording)
 
+  attr_reader :gender
+
   def male?
-    binding.pry
-    user[:gender] == male
+    self[:gender] == "male"
   end
 
+  def female?
+    self[:gender] == "female"
+  end
+
+    # binding.pry
 end
